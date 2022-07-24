9bc8f3a24bb9cb516a4b57d63d63b858273ca5b7
Tony Farrelly <tfarrelly01@gmail.com>
2022-07-24 12:44:50 +0100

    Action and State modification

--- src/Application.js
+++ src/Application.js
@@ -7,21 +7,51 @@ import NewGrudge from './NewGrudge';
 
 import initialState from './initialState';
 
+const GRUDGE_ADD = 'GRUDGE_ADD';
+const GRUDGE_FORGIVE = 'GRUDGE_FORGIVE';
+
 const reducer = (state, action) => {
   console.log({ action });
+  if (action.type === GRUDGE_ADD) {
+    return [ action.payload, ...state ];
+  }
+  if (action.type === GRUDGE_FORGIVE) {
+    console.log(action.type);
+    return state.map(grudge => {
+      if (grudge.id !== action.payload.id) return grudge;
+      return { ...grudge, forgiven: !grudge.forgiven };
+    })
+  }
   return state;
 };
 
 const Application = () => {
   const [grudges, dispatch] = useReducer(reducer, initialState);
 
+  const addGrudge = ({ person, reason }) => {
+    dispatch({
+      type: GRUDGE_ADD,
+      payload: {
+        person,
+        reason,
+        forgiven: false,
+        id: id()
+      }
+    });
+  };
+  /*
   const addGrudge = grudge => {
     grudge.id = id();
     grudge.forgiven = false;
-    // setGrudges([grudge, ...grudges]);
+    setGrudges([grudge, ...grudges]);
   };
+  */
 
   const toggleForgiveness = id => {
+    dispatch({
+      type: GRUDGE_FORGIVE,
+      payload: { id }
+    });
     /*
     setGrudges(
       grudges.map(grudge => {
