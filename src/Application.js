99f9f5a823ef75a9bc283d31e00b11ffa86c9a2a
Tony Farrelly <tfarrelly01@gmail.com>
2022-07-24 13:32:32 +0100

    React.memo and useCallback

--- src/Application.js
+++ src/Application.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useCallback } from 'react';
 
 import id from 'uuid/v4';
 
@@ -11,12 +11,10 @@ const GRUDGE_ADD = 'GRUDGE_ADD';
 const GRUDGE_FORGIVE = 'GRUDGE_FORGIVE';
 
 const reducer = (state, action) => {
-  console.log({ action });
   if (action.type === GRUDGE_ADD) {
     return [ action.payload, ...state ];
   }
   if (action.type === GRUDGE_FORGIVE) {
-    console.log(action.type);
     return state.map(grudge => {
       if (grudge.id !== action.payload.id) return grudge;
       return { ...grudge, forgiven: !grudge.forgiven };
@@ -28,17 +26,20 @@ const reducer = (state, action) => {
 const Application = () => {
   const [grudges, dispatch] = useReducer(reducer, initialState);
 
-  const addGrudge = ({ person, reason }) => {
-    dispatch({
-      type: GRUDGE_ADD,
-      payload: {
-        person,
-        reason,
-        forgiven: false,
-        id: id()
-      }
-    });
-  };
+  const addGrudge = useCallback(
+    ({ person, reason }) => {
+      dispatch({
+        type: GRUDGE_ADD,
+        payload: {
+          person,
+          reason,
+          forgiven: false,
+          id: id()
+        }
+      });
+    }, 
+    [dispatch]
+  );
   /*
   const addGrudge = grudge => {
     grudge.id = id();
@@ -47,20 +48,23 @@ const Application = () => {
   };
   */
 
-  const toggleForgiveness = id => {
-    dispatch({
-      type: GRUDGE_FORGIVE,
-      payload: { id }
-    });
-    /*
-    setGrudges(
-      grudges.map(grudge => {
-        if (grudge.id !== id) return grudge;
-        return { ...grudge, forgiven: !grudge.forgiven };
-      })
+  const toggleForgiveness = useCallback(
+    id => {
+      dispatch({
+        type: GRUDGE_FORGIVE,
+        payload: { id }
+      });
+      /*
+      setGrudges(
+        grudges.map(grudge => {
+          if (grudge.id !== id) return grudge;
+          return { ...grudge, forgiven: !grudge.forgiven };
+        })
     );
     */
-  };
+    }, 
+    [dispatch]
+  );
 
   return (
     <div className="Application">
