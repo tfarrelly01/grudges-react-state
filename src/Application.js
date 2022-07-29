93719a09f81808bbaa5735a68b7c0ca19f1990d1
Tony Farrelly <tfarrelly01@gmail.com>
2022-07-29 17:00:52 +0100

    Implement Undo Reducer

--- src/Application.js
+++ src/Application.js
@@ -2,11 +2,22 @@ import React from 'react';
 
 import Grudges from './Grudges';
 import NewGrudge from './NewGrudge';
+import { GrudgeContext } from './GrudgeContext';
 
 const Application = () => {
+const { undo, isPast } = React.useContext(GrudgeContext);
+
+console.log({ undo, isPast });
+
   return (
     <div className="Application">
       <NewGrudge />
+      <section> 
+        <button disabled={!isPast} onClick={undo}>
+          Undo
+        </button>
+        <button>Redo</button>
+      </section>
       <Grudges />
     </div>
   );
